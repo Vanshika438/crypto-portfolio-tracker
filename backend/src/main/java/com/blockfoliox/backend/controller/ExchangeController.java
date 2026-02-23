aeaa3234d56696932a22a3620e36efe43bfaf25e
Vanshika438 <vanshikasharma01012007@gmail.com>
2026-02-23 20:15:14 +0530

    UI update and exchange error fix

--- backend/src/main/java/com/blockfoliox/backend/controller/ExchangeController.java
+++ backend/src/main/java/com/blockfoliox/backend/controller/ExchangeController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import com.blockfoliox.backend.model.User;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -40,6 +41,11 @@ public class ExchangeController {
         this.userRepository = userRepository;
         this.jwtUtil = jwtUtil;
         this.encryptionService = encryptionService;
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getAllExchanges() {
+        return ResponseEntity.ok(exchangeRepository.findAll());
     }
 
     @PostMapping("/connect")
